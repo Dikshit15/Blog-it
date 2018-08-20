14b965b6c8e3a91c72f7adfd93d3bc7d46dee32f
dikshit <dikshitmaheshwari15@gmail.com>
2018-08-20 16:38:32 +0530

    This is an important task

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
+
+#render function puts together our template blog/post_list.html
