07d32c757d38dd3baecd3591d59a25f4ca5f8501
dikshit <dikshitmaheshwari15@gmail.com>
2018-08-30 19:56:17 +0530

    This is where I uderstood few things.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts': posts})
 
 #render function puts together our template blog/post_list.html
