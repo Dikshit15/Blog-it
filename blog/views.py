e48d141acc951c7be19c918035d287788a1998c8
dikshit <dikshitmaheshwari15@gmail.com>
2018-09-09 10:41:01 +0530

    Add view and templates for new post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 # git commit -a This is an important command.
@@ -7,3 +7,7 @@ def post_list(request):
     return render(request,'blog/post_list.html',{'posts': posts})
 # This is a sample.
 #render function puts together our template blog/post_list.html
+#post.objects.get(pk=pk)
+def post_detail(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
